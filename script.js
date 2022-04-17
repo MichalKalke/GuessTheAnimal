b10f9a68475a1cf0743641fadef21b25c660da4d
kalkemich <56382792+kalkemich@users.noreply.github.com>
2022-04-17 18:48:28 +0200

    Added different music and hidden again btn

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ let songMuted = false;
 document.querySelector('.muteBtn').addEventListener('click', () => {
   const mute = document.getElementById('song');
   if (songMuted === false) {
-    console.log('here');
     mute.muted = true;
     songMuted = true;
     document
@@ -47,6 +46,19 @@ class Animal {
     Object.assign(this, json);
   }
 }
+
+const playEndMusic = async music => {
+  const mute = document.getElementById('song');
+  document.querySelector('.again').classList.add('hidden');
+  mute.muted = true;
+  const delay = ms => new Promise(res => setTimeout(res, ms));
+  const audio = new Audio(music);
+  await delay(500);
+  audio.play();
+  await delay(7000);
+  mute.muted = false;
+  document.querySelector('.again').classList.remove('hidden');
+};
 
 const dataJson = `[{"species": "mammals","animal": "elephant","clues": ["I live a long time", "I have tusks", "My nose is a trunk"]},{"species": "reptiles","animal": "crocodile","clues": ["My jaw is very powerful","I live on the land and in the water","I'm the face of Lacoste"]},{"species": "birds","animal": "penguin","clues": ["I love freezing weather","People think I don't have knees but I have!","I can't fly"]},{"species": "amphibians","animal": "frog","clues": ["I like water", "I croak to myself", "I'm not a toad!"]},{"species": "insects","animal": "mosquito","clues": ["I live mainly in Summer", "Noone likes me", "I'm a vampire!"]},{"species": "arachnids","animal": "scorpio","clues": ["I'm poisonous", "I have tongs", "My tail is a spike"]},{"species": "mammals","animal": "panda","clues": ["I'm a China national", "I'm fluffy", "I eat bamboo"]},{"species": "reptiles","animal": "chameleon","clues": ["Now you can see me, now you can't","My eyes see everything","I can change my color"]},{"species": "birds","animal": "stork","clues": ["I bring you children","In winter I reside in Africa","When I come back you say it's spring"]},{"species": "insects","animal": "bee","clues": ["I'm buzzzzing", "I'm useful", "My name is Maja"]},{"species": "mammals","animal": "lemur","clues": ["Call me Julian, King Julian","I like to move it!","I came from Madagascar"]},{"species": "reptiles","animal": "salamander","clues": ["I'm in spots", "I have a tail", "I'm close to salame"]},{"species": "insects","animal": "mantis","clues": ["My eyes are beautiful","I have 2 swords","I eat my husband for breakfast"]},{"species": "arthropod","animal": "lobster","clues": ["My tail is strong so my paws","My paws are nippers","I'm very yummy"]},{"species": "insects","animal": "bittern","clues": ["Big Boi", "I'm not bumblebee", "Fart is a synomime in Polish"]}]`;
 
@@ -102,13 +114,11 @@ function init() {
 
 const play = function () {
   secretNumber = Math.trunc(Math.random() * 15);
-  console.log(secretNumber);
   if (usedNumbers.length === 0 || usedNumbers.indexOf(secretNumber)) {
     usedNumbers.push(secretNumber);
     console.log('Nice');
     infoSpecies.textContent = 'Species:\n' + animalsArray[secretNumber].species;
     riddle.textContent = animalsArray[secretNumber].clues[clue];
-    console.log(animalsArray[secretNumber].animal);
   } else {
     console.log('Used :(');
     play();
@@ -146,11 +156,14 @@ function gameEnded() {
 
   if (score < highScore && score > 0) {
     displayEndMessage('Nice! Close to beat best! 🙌');
+    playEndMusic('music/nice.mp3');
   } else if (score < 0) {
     displayEndMessage('This was horrible 💫');
+    playEndMusic('music/over.wav');
   } else {
     document.querySelector('.highscore').textContent = score;
     displayEndMessage('👑 AWESOME! NEW HIGHSCORE! 👑');
+    playEndMusic('music/won.wav');
   }
 }
 
